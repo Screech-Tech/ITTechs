a554564e5d4362ad0013ac4e4e88a1249e1de9ee
Oleg Kozlov <oleg.kozlov2020@gmail.com>
2020-06-29 19:33:41 +0300

    Добавьте файлы проекта.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITTechs.Startup))]
+namespace ITTechs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
