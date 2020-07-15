71711cfba8291628df5528027c1dd44e32447e52
Oleg Kozlov <oleg.kozlov2020@gmail.com>
2020-07-16 02:20:07 +0300

    Connection Code First

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace ITTechs.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ITTechs.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(ITTechs.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
