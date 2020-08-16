523f5d7c88710e845783a0d0082083591e69514c
Oleg Kozlov <oleg.kozlov2020@gmail.com>
2020-08-16 19:21:28 +0300

    Users & Subscriptions - Altering the Create Actions and View

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -498,5 +498,41 @@ namespace ITTechs.Controllers
 
             return View(users);
         }
+
+        [Authorize(Roles ="Admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    IsActive = true,
+                    Registered = DateTime.Now,
+                    EmailConfirmed = true
+                };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Account");
+                }
+                AddErrors(result);
+            }
+
+            // Появление этого сообщения означает наличие ошибки; повторное отображение формы
+            return View(model);
+        }
     }
 }
