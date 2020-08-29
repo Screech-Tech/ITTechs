7ada40b112f3cbbcb274d63e738032c640f0d65e
Oleg Kozlov <oleg.kozlov2020@gmail.com>
2020-08-29 19:00:09 +0300

    Adding the Subscriptions Action and View

--- Extensions/IdentityExtensions.cs
+++ Extensions/IdentityExtensions.cs
@@ -20,13 +20,15 @@ namespace ITTechs.Extensions
         public static async Task GetUsers(this List<UserViewModel> users)
         {
             var db = ApplicationDbContext.Create();
-            users.AddRange(await (from u in db.Users
-                                  select new UserViewModel
-                                  {
-                                      Id = u.Id,
-                                      Email = u.Email,
-                                      FirstName = u.FirstName
-                                  }).OrderBy(o => o.Email).ToListAsync());
+            {
+                users.AddRange(await (from u in db.Users
+                                      select new UserViewModel
+                                      {
+                                          Id = u.Id,
+                                          Email = u.Email,
+                                          FirstName = u.FirstName
+                                      }).OrderBy(o => o.Email).ToListAsync());
+            }
         }
     }
 }
