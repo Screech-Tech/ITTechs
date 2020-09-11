e2676a3898c724a6b5c33d3bcdb5b9ca7c7a8079
Oleg Kozlov <oleg.kozlov2020@gmail.com>
2020-09-11 17:55:59 +0300

    Display products - Adding the GetSubscriptionIdsAsync method

---
+++ Extensions/ThumbnailExtensions.cs
@@ -0,0 +1,31 @@
+﻿using ITTechs.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ITTechs.Extensions
+{
+    public static class ThumbnailExtensions
+    {
+        private static async Task<List<int>> GetSubscriptionIdsAsync(
+            string userId = null, ApplicationDbContext db = null)
+        {
+            try
+            {
+                if (userId == null) return new List<int>();
+                if (db == null) db = ApplicationDbContext.Create();
+
+                return await (
+                    from us in db.UserSubscriptions
+                    where us.UserId.Equals(userId)
+                    select us.SubscriptionId).ToListAsync();
+            }
+            catch { }
+
+            return new List<int>();
+        }
+    }
+}
