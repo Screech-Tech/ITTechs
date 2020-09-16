3fc75723e30770e03313c8dbb2d0f85545ac7a47
Oleg Kozlov <oleg.kozlov2020@gmail.com>
2020-09-16 11:24:20 +0300

    Product Page - Adding the ProductSectionEqualityComparer class

---
+++ Compares/ProductSectionEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using ITTechs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITTechs.Compares
+{
+    public class ProductSectionEqualityComparer : IEqualityComparer<ProductSection>
+    {
+        public bool Equals(ProductSection section1, ProductSection section2)
+        {
+            return section1.Id.Equals(section2.Id);
+        }
+
+        public int GetHashCode(ProductSection section)
+        {
+            return (section.Id).GetHashCode();
+        }
+    }
+}
